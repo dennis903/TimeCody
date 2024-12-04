0aae7d51694644a08b378b9166f18870281268e0
이형진 <hyeolee@zempot.com>
2024-12-04 23:26:26 +0900

    Co-authored-by: Beefruit <Beefruit@users.noreply.github.com>

--- src/container/Modals/SidebarPlusModal/SidebarPlusModal.container.tsx
+++ src/container/Modals/SidebarPlusModal/SidebarPlusModal.container.tsx
@@ -9,28 +9,26 @@ const SidebarPlusModalContainer: FC = () => {
   const { sidebarPlusState } = useModalStore();
 
   return (
-    sidebarPlusState.isOpen && (
-      <ModalComponent>
-        <div className="sidebar-modal-plus">
-          <header className="sidebar-modal__header">
-            <button type="button" className="icon-btn">
-              <IconComponent icon="icon-close" />
-            </button>
-            <button type="button" className="icon-btn">
-              <IconComponent icon="icon-check-complete" />
-            </button>
-          </header>
-          <input
-            className="sidebar-modal-plus__input"
-            type="text"
-            name="new"
-            id="new"
-            placeholder="새 카테고리 이름을 입력하세요"
-          />
-          <button className="sidebar-modal__delete-btn">삭제</button>
-        </div>
-      </ModalComponent>
-    )
+    <ModalComponent>
+      <div className="sidebar-modal-plus">
+        <header className="sidebar-modal__header">
+          <button type="button" className="icon-btn">
+            <IconComponent icon="icon-close" />
+          </button>
+          <button type="button" className="icon-btn">
+            <IconComponent icon="icon-check-complete" />
+          </button>
+        </header>
+        <input
+          className="sidebar-modal-plus__input"
+          type="text"
+          name="new"
+          id="new"
+          placeholder="새 카테고리 이름을 입력하세요"
+        />
+        <button className="sidebar-modal__delete-btn">삭제</button>
+      </div>
+    </ModalComponent>
   );
 };
 
