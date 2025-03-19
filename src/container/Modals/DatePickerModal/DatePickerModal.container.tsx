d98313ba0d9166db3805b6f0d3be4cd8de9a1d97
이형진 <hyeolee@zempot.com>
2025-03-19 21:59:22 +0900

    Co-authored-by: Beefruit <Beefruit@users.noreply.github.com>

--- src/container/Modals/DatePickerModal/DatePickerModal.container.tsx
+++ src/container/Modals/DatePickerModal/DatePickerModal.container.tsx
@@ -1,16 +1,72 @@
 import { type FC, useState } from 'react';
+import classNames from 'classnames/bind';
+import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/locale';
 
+import IconComponent from '@/components/Icon/Icon.component';
 import ModalComponent from '@/components/modal/Modal.component';
 import useDatePickerModalStore from '@/store/DatePickerModal.store';
 
+import 'react-datepicker/dist/react-datepicker.css';
 import './DatePickerModal.container.css';
+
+const cx = classNames.bind({});
 
 const DatePickerModalContainer: FC = () => {
   const { datePickerModalState, toggleDatePickerModal } = useDatePickerModalStore();
+  const [activeTimeTable, setActiveTimeTable] = useState(true);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleStartDateChange = (date: Date) => {
+    console.log(date);
+    setStartDate(date);
+  };
 
   return (
-    <ModalComponent isOpen={true} zIndex={20000} onClose={() => toggleDatePickerModal(false)}>
-      <div className="date-picker-modal"></div>
+    <ModalComponent isOpen={false} zIndex={20000} onClose={() => toggleDatePickerModal(false)}>
+      <div className="date-picker-modal">
+        <div className="date-picker-modal__header">
+          <div
+            className={cx('time-table', {
+              on: activeTimeTable,
+            })}
+            onClick={() => setActiveTimeTable(true)}
+          >
+            <span className="time-table__header">시작</span>
+            <span className="time-table__time">3월 19일</span>
+            <span className="time-table__clock">오후 8:00</span>
+          </div>
+          <IconComponent
+            className="icon-btn"
+            icon="icon-arrow"
+            style={{
+              width: '1.5rem',
+            }}
+          />
+          <div
+            className={cx('time-table', {
+              on: !activeTimeTable,
+            })}
+            onClick={() => setActiveTimeTable(false)}
+          >
+            <span className="time-table__header">종료</span>
+            <span className="time-table__time">3월 19일</span>
+            <span className="time-table__clock">오후 10:00</span>
+          </div>
+        </div>
+        <div className="date-picker-modal__main">
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => handleStartDateChange(date as Date)}
+            startDate={startDate}
+            dateFormatCalendar={'yyyy년 MM월'}
+            inline
+            showDisabledMonthNavigation
+            locale={ko}
+          />
+        </div>
+      </div>
     </ModalComponent>
   );
 };
--- src/view/layout/app.layout.tsx
+++ src/view/layout/app.layout.tsx
@@ -2,6 +2,7 @@ import { type FC } from 'react';
 import SidebarPlusModalContainer from '@/container/Modals/SidebarPlusModal/SidebarPlusModal.container';
 import CalendarModalContainer from '@/container/Modals/CalendarModal/CalendarModal.container';
 import AdditionalModalContainer from '@/container/Modals/AdditionalModal/AdditionalModal.container';
+import DatePickerModalContainer from '@/container/Modals/DatePickerModal/DatePickerModal.container';
 
 interface IAppLayoutProps {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ const AppLayout: FC<IAppLayoutProps> = (props) => {
       <SidebarPlusModalContainer />
       <CalendarModalContainer />
       <AdditionalModalContainer />
+      <DatePickerModalContainer />
     </div>
   );
 };
